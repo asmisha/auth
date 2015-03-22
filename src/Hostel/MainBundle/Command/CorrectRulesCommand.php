085b92a4db9e3a892d8b542ea9379124f8d3dc6c
Michael Astashkevich <asmisha@tut.by>
2015-03-22 15:03:24 +0300

    fix

--- src/Hostel/MainBundle/Command/CorrectRulesCommand.php
+++ src/Hostel/MainBundle/Command/CorrectRulesCommand.php
@@ -24,68 +24,68 @@ class CorrectRulesCommand extends ContainerAwareCommand
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
+		/** @var IpMac $ipmac */
+		$ipmac = $this->getContainer()->get('ipmac');
+
 		$hostels = array_keys($this->getContainer()->getParameter('hostel_addresses'));
+		$rules = array();
 
-		foreach($hostels as $h) {
-			/** @var IpMac $ipmac */
-			$ipmac = $this->getContainer()->get('ipmac');
-			$rules = $ipmac->listRules($h);
-
-			/** @var EntityManager $em */
-			$em = $this->getContainer()->get('doctrine')->getManager();
-
-			/** @var User[] $users */
-			$users = $em->getRepository('HostelMainBundle:User')->createQueryBuilder('u')
-				->where('u.hostel = :hostel AND u.ip IS NOT NULL AND u.mac IS NULL')
-				->setParameter('hostel', $h)
-				->getQuery()
-				->getResult()
-			;
-
-			foreach($users as $u){
-				if($mac = $ipmac->getMac($u)){
-					$u->setMac($mac);
-				}else{
-					$u
-						->setIp(null)
-						->setBanned(true)
-					;
-				}
-			}
-			$em->flush();
-
-			/** @var User[] $users */
-			$users = $em->getRepository('HostelMainBundle:User')->findBy(array(
-				'hostel' => $h,
-				'banned' => false,
-			));
-
-			foreach ($users as $u) {
-				$k = strtoupper(sprintf('%s,%s', $u->getIp(), $u->getMac()));
-				if (isset($rules[$k])) {
-					$rules[$k] = true;
-				} else {
-					$output->writeln(sprintf('Unbanning user %d %s %s %s %s', $u->getId(), $u->getFirstname(), $u->getLastname(), $u->getIp(), $u->getMac()));
-
-					$ipmac->unban($u);
-				}
-			}
-
-			foreach ($rules as $k => $i) {
-				if (!$i) {
-					$data = explode(',', $k);
-					if(count($data) != 2){
-						continue;
-					}
-					list($ip, $mac) = $data;
-
-					$output->writeln(sprintf('Banning %s %s', $ip, $mac));
-
-					$ipmac->banIpMac($ip, $mac);
-				}
-			}
-
-			$em->flush();
+		foreach($hostels as $h){
+			$rules = array_merge($rules, $ipmac->listRules($h));
 		}
+
+		/** @var EntityManager $em */
+		$em = $this->getContainer()->get('doctrine')->getManager();
+
+		/** @var User[] $users */
+		$users = $em->getRepository('HostelMainBundle:User')->createQueryBuilder('u')
+			->where('u.ip IS NOT NULL AND u.mac IS NULL')
+			->getQuery()
+			->getResult()
+		;
+
+		foreach($users as $u){
+			if($mac = $ipmac->getMac($u)){
+				$u->setMac($mac);
+			}else{
+				$u
+					->setIp(null)
+					->setBanned(true)
+				;
+			}
+		}
+		$em->flush();
+
+		/** @var User[] $users */
+		$users = $em->getRepository('HostelMainBundle:User')->findBy(array(
+			'banned' => false,
+		));
+
+		foreach ($users as $u) {
+			$k = strtoupper(sprintf('%s,%s', $u->getIp(), $u->getMac()));
+			if (isset($rules[$k])) {
+				$rules[$k] = true;
+			} else {
+				$output->writeln(sprintf('Unbanning user %d %s %s %s %s', $u->getId(), $u->getFirstname(), $u->getLastname(), $u->getIp(), $u->getMac()));
+
+				$ipmac->unban($u);
+			}
+		}
+
+		foreach ($rules as $k => $i) {
+			if (!$i) {
+				$data = explode(',', $k);
+				if(count($data) != 2){
+					continue;
+				}
+				list($ip, $mac) = $data;
+
+				$output->writeln(sprintf('Banning %s %s', $ip, $mac));
+
+				$ipmac->banIpMac($ip, $mac);
+			}
+		}
+
+		$em->flush();
 	}
 }
